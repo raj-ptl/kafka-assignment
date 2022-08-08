b9e0da005fd215ec83e36f8b43b8cfcb32717ed1
raj-ptl <raj.dpatel@swiggy.in>
2022-08-08 15:00:39 +0530

    repo init

---
+++ consumer_2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+
+	topic := "example-123"
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		GroupID: "group-1",
+		Topic:   topic,
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			log.Fatal("Failed to read message : ", err)
+			break
+		}
+
+		fmt.Printf("group-1 consumer-2 : Message at offset %d : %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("Failed to close reader : ", err)
+	}
+
+}
---
+++ producer/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	w := &kafka.Writer{
+		Addr:                   kafka.TCP("localhost:9092"),
+		Topic:                  "example-123",
+		RequiredAcks:           kafka.RequireAll,
+		AllowAutoTopicCreation: true,
+		Async:                  true,
+		Completion: func(messages []kafka.Message, err error) {
+
+			if err != nil {
+				fmt.Println("Compl. error : ", err)
+				return
+			}
+
+			for _, val := range messages {
+				fmt.Printf("Messages sent, offset %d, key %s, val %s\n", val.Offset, val.Key, val.Value)
+			}
+		},
+	}
+
+	err := w.WriteMessages(ctx,
+		kafka.Message{
+			Key:   []byte("dummyKey"),
+			Value: []byte("dummyValue"),
+		},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to write messages : ", err)
+	}
+
+	if err := w.Close(); err != nil {
+		log.Fatal("Failed to close writer: ", err)
+	}
+}
